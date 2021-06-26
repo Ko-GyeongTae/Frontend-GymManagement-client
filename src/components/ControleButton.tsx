fb6ef3c9fac71c5f9e4b5cb42c00aa9ae87b5e0e
Ko-GyeongTae <kokt0203@naver.com>
2021-06-26 16:34:47 +0900

    <ADD> Controle Button

---
+++ src/components/ControleButton.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const ControleButton = () => {
+
+  return (
+    <div>
+      <h1>ControleButton</h1>
+      <h2>controle + button</h2>
+    </div>
+  );
+}
+
+export default ControleButton;
