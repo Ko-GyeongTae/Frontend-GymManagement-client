287b1cce3a35e2dc114033993a9b180a414fc55a
Ko-GyeongTae <kokt0203@naver.com>
2021-06-26 16:36:33 +0900

    <ADD> SideBar

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const SideBar = () => {
+
+  return (
+    <div>
+      <h1>SearchBar</h1>
+      <h2>searchbar + button</h2>
+    </div>
+  );
+}
+
+export default SideBar;
