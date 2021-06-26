135ef04a5dbff957d2468e83f12f1c33e2d8fcd6
Ko-GyeongTae <kokt0203@naver.com>
2021-06-26 16:35:31 +0900

    <ADD> Product

---
+++ src/components/Product.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const ProductBox = () => {
+
+  return (
+    <div>
+      <h1>ProductBox</h1>
+      <h2>productbar + button</h2>
+    </div>
+  );
+}
+
+export default ProductBox;
