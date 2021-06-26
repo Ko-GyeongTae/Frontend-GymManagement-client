f294a8019f27894031b8067d05227149ea792483
Ko-GyeongTae <kokt0203@naver.com>
2021-06-26 16:36:12 +0900

    <ADD> SearchBar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const SearchBar = () => {
+
+  return (
+    <div>
+      <h1>SearchBar</h1>
+      <h2>searchbar + button</h2>
+    </div>
+  );
+}
+
+export default SearchBar;
