681422e35634c530e74cee934f6e58999ea189bf
Ko-GyeongTae <kokt0203@naver.com>
2021-06-29 14:17:00 +0900

    <ADD> axios

--- src/lib/auth/login.ts
+++ src/lib/auth/login.ts
@@ -1,6 +1,16 @@
-const login = () => {
+import axios from "axios";
 
-    return {}
+const login = async(args: login_Req):Promise<any> => {
+    await axios.post(`${process.env.NEXT_PUBLIC_BASEURL}/api/login`, args)
+    .then(res => {
+        console.log(res.data);
+        return res.data
+    })
+    
 }
 
 export default login;
+
+export interface login_Req {
+    phone: string
+}
--- src/lib/auth/signUp.ts
+++ src/lib/auth/signUp.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const signUp = async(args: signUp_Req):Promise<any> => {
-    await axios.post(`${process.env.NEXT_PUBLIC_BASEURL}/api/auth`)
+    await axios.post(`${process.env.NEXT_PUBLIC_BASEURL}/api/auth`, args)
     .then(res => {
         console.log(res.data);
         return res.data;
