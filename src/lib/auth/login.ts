46b9d291a09b8b1b1e1c9eff202a81d1195b5df8
Ko-GyeongTae <kokt0203@naver.com>
2021-06-29 20:28:37 +0900

    <EDIT> axios flow

--- src/lib/auth/login.ts
+++ src/lib/auth/login.ts
@@ -4,9 +4,11 @@ const login = async(args: login_Req):Promise<any> => {
     await axios.post(`${process.env.NEXT_PUBLIC_BASEURL}/api/login`, args)
     .then(res => {
         console.log(res.data);
-        return res.data
+        return res.data;
     })
-    
+    .catch(e => {
+        console.error(e);
+    })
 }
 
 export default login;
