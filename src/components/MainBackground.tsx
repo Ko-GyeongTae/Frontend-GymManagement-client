20cb2d9e46646e96ac6078231a92b8321076b2fe
Ko-GyeongTae <kokt0203@naver.com>
2021-06-30 22:42:29 +0900

    <ADD> MainBackground

---
+++ src/components/MainBackground.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const MainBackground = (props: any) => {
+
+    return (
+        <div>
+            <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="1637" height="906.818" viewBox="0 0 1637 906.818">
+                <defs>
+                    <clipPath id="clip-path">
+                        <rect width="1637" height="906.818" fill="none" />
+                    </clipPath>
+                </defs>
+                <g id="그룹_1" data-name="그룹 1" transform="translate(0 1.409)">
+                    <g id="반복_그리드_1" data-name="반복 그리드 1" transform="translate(0 -1.409)" clip-path="url(#clip-path)">
+                        <g transform="translate(0 1.409)">
+                            <path id="패스_26" data-name="패스 26" d="M-2788.762,1601l-254.8,307.006L-2160,2141.644V1601Z" transform="translate(3797 -1601)" fill="#d4d1ce" />
+                            <path id="패스_25" data-name="패스 25" d="M-5350.076,2505l724.663-906.818h-929.133V2505Z" transform="translate(5554.545 -1599.591)" fill="#8c837a" />
+                        </g>
+                    </g>
+                </g>
+            </svg>
+        </div>
+    );
+}
+
+export default MainBackground;
