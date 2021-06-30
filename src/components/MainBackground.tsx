a965652f85b0bfdff18d873c71c54f2f2d49b9d6
Ko-GyeongTae <kokt0203@naver.com>
2021-06-30 22:49:18 +0900

    <EDIT> clip-path -> clipPath

--- src/components/MainBackground.tsx
+++ src/components/MainBackground.tsx
@@ -11,7 +11,7 @@ const MainBackground = (props: any) => {
                     </clipPath>
                 </defs>
                 <g id="그룹_1" data-name="그룹 1" transform="translate(0 1.409)">
-                    <g id="반복_그리드_1" data-name="반복 그리드 1" transform="translate(0 -1.409)" clip-path="url(#clip-path)">
+                    <g id="반복_그리드_1" data-name="반복 그리드 1" transform="translate(0 -1.409)" clipPath="url(#clip-path)">
                         <g transform="translate(0 1.409)">
                             <path id="패스_26" data-name="패스 26" d="M-2788.762,1601l-254.8,307.006L-2160,2141.644V1601Z" transform="translate(3797 -1601)" fill="#d4d1ce" />
                             <path id="패스_25" data-name="패스 25" d="M-5350.076,2505l724.663-906.818h-929.133V2505Z" transform="translate(5554.545 -1599.591)" fill="#8c837a" />
