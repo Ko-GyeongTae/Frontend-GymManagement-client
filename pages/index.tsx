425618bb1ac1f19903fc7c1db0fde0f01f20e396
Ko-GyeongTae <kokt0203@naver.com>
2021-06-30 23:48:09 +0900

    <EDIT> login ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,16 @@ import MainBackground from "../src/components/MainBackground";
 import LoginButton from '../src/components/LoginButton';
 
 const Home = () => {
-
   return (
+    <>
+    <Head>
+      <title>홈</title>
+    </Head>
     <div style={{alignItems: 'center', overflowX: 'hidden', overflowY: 'hidden', width: '100%', height: '100%'}}> 
       <MainBackground/>
       <LoginButton backgroundColor={'#8c837a'} fontColor={'#ffffff'} bottom={79} right={70}/>
     </div>
+    </>
   );
 }
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import Head from "next/head";
+import LoginButton from '../src/components/LoginButton';
 
 const Login = () => {
 
   return (
-    <div>
-      <h1>Login</h1>
-      <h2>간편로그인</h2>
+    <div style={{width: '100%', height: '100vh'}}>
+      <div style={{display: 'inline-flex', width: '100%', height: '15%', textAlign: 'center'}}>
+          <div style={{width: '88.5%'}}>
+            <p style={{verticalAlign: 'middle', fontSize: 33}}>휴대전화번호를 눌러주세요.</p>
+          </div>
+          <div style={{width: '11.5%'}}>
+            <p style={{verticalAlign: 'middle', fontSize: 33}}>취소</p>
+          </div>
+      </div>
+      <div style={{width: '100%', height: '85%', display: 'flex'}}>
+          <div style={{width: '40%', backgroundColor: '#8C837A', alignItems: 'center'}}>
+            <LoginButton backgroundColor={'white'} fontColor={'#8C837A'} left={70} top={200}/>
+          </div>
+          <div style={{width: '60%', border: 1, borderStyle: 'solid', borderColor: 'black'}}>
+            <h4>right</h4>
+          </div>
+      </div>
     </div>
   );
 }
