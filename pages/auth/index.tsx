6e675e0e0ec4fc0344dca892e48a7d77b179382a
Ko-GyeongTae <kokt0203@naver.com>
2021-06-30 22:43:21 +0900

    <ADD> auth

---
+++ pages/auth/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const Home = () => {
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <h2>간편로그인</h2>
+    </div>
+  );
+}
+
+export default Home;
