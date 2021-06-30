38a7fc0862c263616c8ebfab2989d20f7708d94e
Ko-GyeongTae <kokt0203@naver.com>
2021-06-30 22:45:52 +0900

    <EDIT> a tag -> link tag

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Link from 'next/link';
 
 const LoginButton = (props: any) => {
     return (
-        <a href='/login'>
+        <Link href='/login'>
             <div style={{width: 551, height: 97, backgroundColor: props.backgroundColor, position: 'absolute', right: props.right, bottom: props.bottom, borderRadius: 49, textAlign: 'center'}}>
                 <p style={{fontSize: 33, fontFamily: 'Playbill', color: props.fontColor}}>간편로그인</p>
             </div>
-        </a>
+        </Link>
     )
 }
 
