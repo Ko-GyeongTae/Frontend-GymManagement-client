8834cdc948476649729ee2f2f245969c5f2944fd
Ko-GyeongTae <kokt0203@naver.com>
2021-06-30 22:42:50 +0900

    <ADD> login

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Head from "next/head";
+
+const Login = () => {
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <h2>간편로그인</h2>
+    </div>
+  );
+}
+
+export default Login;
