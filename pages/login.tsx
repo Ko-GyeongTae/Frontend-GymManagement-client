95143a722d7da2cd35f09ba9bf5a58426548d584
Ko-GyeongTae <kokt0203@naver.com>
2021-07-01 16:43:20 +0900

    <EDIT> style -> css module

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import LoginButton from '../src/components/LoginButton';
 import Keypad from "../src/components/Keypad";
+import style from './login.module.css';
 
 const Login = () => {
     let phoneNum = [];
@@ -14,24 +15,24 @@ const Login = () => {
     });
 
     return (
-        <div style={{ width: '100%', height: '100vh' }}>
-            <div style={{ display: 'inline-flex', width: '100%', height: '15%', textAlign: 'center' }}>
-                <div style={{ width: '88.5%' }}>
-                    <p style={{ verticalAlign: 'middle', fontSize: 33 }}>휴대전화번호를 눌러주세요.</p>
+        <div className={style.Container}>
+            <div className={style.Header}>
+                <div className={style.TitleBox}>
+                    <p className={style.TitleText}>휴대전화번호를 눌러주세요.</p>
                 </div>
-                <div style={{ width: '11.5%' }}>
-                    <p style={{ verticalAlign: 'middle', fontSize: 33 }}>취소</p>
+                <div className={style.CancelBox}>
+                    <p className={style.TitleText}>취소</p>
                 </div>
             </div>
-            <div style={{ width: '100%', height: '85%', display: 'flex' }}>
-                <div style={{ width: '40%', backgroundColor: '#8C837A', alignItems: 'center' }}>
+            <div className={style.Body}>
+                <div className={style.LeftBar}>
                     <LoginButton backgroundColor={'white'} fontColor={'#8C837A'} left={70} top={200} />
                 </div>
-                <div style={{ width: '60%', border: 1, borderStyle: 'solid', borderColor: 'black' }}>
-                    <div style={{ width: '100%', height: '34%', textAlign: 'center' }}>
+                <div className={style.RightContainer}>
+                    <div className={style.NumberBox}>
                        
                     </div>
-                    <div style={{ width: '100%', height: '66%', border: 1, borderStyle: 'solid', borderColor: 'black' }}>
+                    <div className={style.KeypadBox}>
                         <Keypad onClick={(phoneNum) => getNumber(phoneNum)}/>
                     </div>
                 </div>
