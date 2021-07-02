21c5af5211c0587b86585b217fc2765a379fdbdb
Ko-GyeongTae <kokt0203@naver.com>
2021-07-02 13:42:50 +0900

    <EDIT> try to connect css.module

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,9 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
+declare module "*.module.css" {
+    const classes: { [key: string]: string}
+    export default classes
+}
+
+declare module "*.module.scss" {
+    const classes: { [key: string]: string}
+    export default classes
+}
