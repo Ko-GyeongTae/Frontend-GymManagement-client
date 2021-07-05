bdcf59909d04f1800dc3fdf8caf9fae020cbf52d
Ko-GyeongTae <kokt0203@naver.com>
2021-07-06 08:15:29 +0900

    <EDIT> UI

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
-import setToken from '../../../src/lib/locallib/setToken';
 
 const login = async (args: login_Req): Promise<any> => {
     let headers = {
         'Content-Type': 'application/json;charset=UTF-8',
         "Access-Control-Allow-Origin": "*",
     };
+
+    let accessToken = "";
     /*
     await axios.post(`${process.env.NEXT_PUBLIC_BASEURL}/api/login`, args)
     .then(res => {
@@ -17,18 +18,19 @@ const login = async (args: login_Req): Promise<any> => {
     })*/
     console.log(args);
     await axios.post('http://localhost:5000/auth/login', {
-        'name': 'taegyeong3',
-        'password': 'hello'
+        'name': args.phone,
+        'password': args.phone
     }, {
         headers: headers
     })
     .then(res => {
-        console.log(res.data);
-        setToken(res.data.access_token);
+        accessToken = res.data.access_token
     })
     .catch(e => {
-        console.log(e);
+        alert('로그인 실패');
+        throw new Error('로그인 정보가 잘못되었습니다.');
     })
+    return accessToken;
 }
 
 export default login;
