d337da885adae9003c87097a490037ae1ab7e21c
KoGyeongtae <kokt0203@naver.com>
2021-08-03 17:57:22 +0900

    <EDIT> signup

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AuthHeader from "../../src/components/AuthHeader";
+import styles from './signup.module.css';
 
 const Home = () => {
-
+    useEffect(() => {
+        console.log(window.innerHeight);
+        console.log(window.outerHeight);
+    })
     return (
-        <h1>Hello</h1>
+        <div className={styles.Container}>
+            <AuthHeader />
+            <div className={styles.MainBox}>
+                <p className={styles.Title}>JOIN US</p>
+                <div className={styles.SubBox}>
+                    <p className={styles.Subtitle}>회원정보를 입력해주세요</p>
+                    <div className={styles.ThirdBox}>
+                        <p>hello</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
