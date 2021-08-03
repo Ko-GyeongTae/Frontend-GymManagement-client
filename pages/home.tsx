c546bfdac3de676f7a6fd5dbb2f786b9d1326f52
KoGyeongtae <kokt0203@naver.com>
2021-08-03 16:36:49 +0900

    <ADD> home page

---
+++ pages/home.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Head from "next/head";
+import HomeBackground from "../src/components/HomeBackground";
+import LoginButton from '../src/components/LoginButton';
+import styles from './home.module.css';
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Wim-Gym | Home</title>
+      </Head>
+      <div className={styles.Container}>
+        <HomeBackground />
+        <LoginButton placeholder={'회원가입'} linkTo={'/signup'} backgroundColor={'#8c837a'} fontColor={'#ffffff'} bottom={75} right={70} />
+        <LoginButton placeholder={'간편로그인'} linkTo={'/login'} backgroundColor={'#8c837a'} fontColor={'#ffffff'} bottom={166} right={70} />
+      </div>
+    </>
+  );
+}
+
+export default Home;
