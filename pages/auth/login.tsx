1ec3c610c4c8c4937d7258bafb2e5dad7eae29b4
KoGyeongtae <kokt0203@naver.com>
2021-08-04 19:18:54 +0900

    <EDIT> login page

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import LoginButton from '../../src/components/LoginButton';
+import styles from './login.module.css';
 import Link from 'next/link';
 import login from "../api/auth/login";
 import { useCookies } from "react-cookie";
@@ -19,97 +19,97 @@ const Login = () => {
         console.log('rerender');
     });
 
-    return (
+    return ( 
         <>
             <Head>
                 <title>Wim-Gym | Login</title>
             </Head>
-            <div style={{ width: '100%', height: '100vh', overflowX: 'hidden' }}>
-                <div style={{ display: 'flex', width: '100%', height: '10%', textAlign: 'center', marginTop: '5%' }}>
-                    <div style={{ width: '89%' }}>
-                        <p style={{ verticalAlign: 'middle', fontSize: 33 }}>휴대전화번호를 눌러주세요.</p>
+            <div className={styles.Container}>
+                <div className={styles.Header}>
+                    <div className={styles.TitleBox}>
+                        <p className={styles.TitleText}>휴대전화번호를 눌러주세요.</p>
                     </div>
-                    <div style={{ width: '11%' }}>
-                        <p style={{ verticalAlign: 'middle', fontSize: 33 }}>취소</p>
+                    <div className={styles.CancelBox}>
+                        <p className={styles.CancelText}>취소</p>
                     </div>
                 </div>
-                <div style={{ width: '100%', height: '80vh', display: 'flex', position: 'fixed', left: 0, bottom: 0, right: 0 }}>
-                    <div style={{ width: '40%', height: 'auto', backgroundColor: '#8C837A', alignItems: 'center' }}/>
-                    <div style={{ width: '60%', height: '100%' }}>
-                        <div style={{ width: '100%', height: '30%', textAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }}>
-                            <div style={{ width: '100%', height: '40%' }}>
-                                <p style={{ fontSize: 60 }}>{phoneNum.slice(0, 3) + (phoneNum.length > 3 ? '-' : "") + phoneNum.slice(3, 7) + (phoneNum.length > 7 ? '-' : "") + phoneNum.slice(7, 11)}</p>
+                <div className={styles.Body}>
+                    <div className={styles.LeftBar}/>
+                    <div className={styles.RightContainer}>
+                        <div className={styles.NumberBox}>
+                            <div className={styles.NumberInnerBox}>
+                                <p className={styles.NumberInnerText}>{phoneNum.slice(0, 3) + (phoneNum.length > 3 ? '-' : "") + phoneNum.slice(3, 7) + (phoneNum.length > 7 ? '-' : "") + phoneNum.slice(7, 11)}</p>
                             </div>
-                            <div style={{ width: '100%', height: '20%', position: 'relative' }}>
-                                <p style={{ fontSize: '100%' }}><Link href='/' passHref>이용약관</Link>과 <Link href='/'>개인정보취급방침</Link>에 동의하시면 휴대전화 번호 입력 후 아래 확인 버튼을 터치하세요.</p>
+                            <div className={styles.NumberInnerNoticeBox}>
+                                <p className={styles.NumberInnerNoticeText}><Link href='/' passHref>이용약관</Link>과 <Link href='/'>개인정보취급방침</Link>에 동의하시면 휴대전화 번호 입력 후 아래 확인 버튼을 터치하세요.</p>
                             </div>
                         </div>
-                        <div style={{ width: 'auto', height: '70%', border: 1, borderStyle: 'solid', borderColor: '#707070' }}>
-                            <div style={{ display: 'table', width: '100%', height: '100%' }}>
-                                <div style={{ display: 'table-row' }}>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                        <div className={styles.KeypadBox}>
+                            <div className={styles.KeypadGrid}>
+                                <div className={styles.KeypadRow}>
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('1');
                                     }}>
                                         <h1>1</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('2');
                                     }}>
                                         <h1>2</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('3');
                                     }}>
                                         <h1>3</h1>
                                     </div>
                                 </div>
-                                <div style={{ display: 'table-row' }}>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                <div className={styles.KeypadRow}>
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('4');
                                     }}>
                                         <h1>4</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('5');
                                     }}>
                                         <h1>5</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('6');
                                     }}>
                                         <h1>6</h1>
                                     </div>
                                 </div>
-                                <div style={{ display: 'table-row' }}>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                <div className={styles.KeypadRow}>
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('7');
                                     }}>
                                         <h1>7</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('8');
                                     }}>
                                         <h1>8</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('9');
                                     }}>
                                         <h1>9</h1>
                                     </div>
                                 </div>
-                                <div style={{ display: 'table-row' }}>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                <div className={styles.KeypadRow}>
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         setPhoneNum(phoneNum.slice(0, -1));
                                     }}>
                                         <h1>지우기</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={() => {
+                                    <div className={styles.KeypadCell} onClick={() => {
                                         InputNumber('0');
                                     }}>
                                         <h1>0</h1>
                                     </div>
-                                    <div style={{ display: 'table-cell', width: '33%', height: '20%', textAlign: 'center', fontSize: 20, verticalAlign: 'center', border: 1, borderStyle: 'solid', borderColor: '#707070' }} onClick={async() => {
-                                        let token = await login({phone: phoneNum})
+                                    <div className={styles.KeypadCell} onClick={async () => {
+                                        let token = await login({ phone: phoneNum })
                                         console.log(token);
                                         setCookie('accessToken', token, { path: '/' });
                                     }}>
